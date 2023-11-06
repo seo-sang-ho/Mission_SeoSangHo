0ab2d550700195e54135a84568116932b44fda2c
seo-sang-ho <rocter2049@naver.com>
2023-11-06 10:23:48 +0900

    feat : add App registration function

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -8,9 +8,18 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("== 명언 앱 ==");
+
         while(true){
-            System.out.println("== 명언 앱 ==");
-            String command = sc.next();
+            System.out.print("명언) ");
+            String command = sc.nextLine();
+
+            if(command.equals("등록")){
+                System.out.print("명언 : ");
+                String famous_Saying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+            }
 
             if(command.equals("종료")){
                 break;
